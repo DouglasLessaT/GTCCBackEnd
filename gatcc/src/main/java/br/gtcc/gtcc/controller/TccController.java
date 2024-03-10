2173bc110b7f9ebbdaa9f261a49e6c01961968cf
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-10 19:23:12 -0300

    Update rotas of tcc controller

--- gatcc/src/main/java/br/gtcc/gtcc/controller/TccController.java
+++ gatcc/src/main/java/br/gtcc/gtcc/controller/TccController.java
@@ -14,16 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.gtcc.gtcc.model.Tcc;
+import br.gtcc.gtcc.model.nitriteid.Tcc;
 import br.gtcc.gtcc.services.impl.nitritedb.TccService;
 
 
 @RestController
-@RequestMapping("/api/tccs")
+@RequestMapping("/v1/tccs")
 public class TccController {
+ 
  @Autowired
  private TccService tccService;
- @PostMapping
+ 
+    @PostMapping("/create")
     public ResponseEntity<Tcc> createTcc(@RequestBody Tcc tcc) {
         Tcc createdTcc = tccService.createTcc(tcc);
         if (createdTcc != null) {
@@ -33,7 +35,7 @@ public class TccController {
         }
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<Tcc> updateTcc(@PathVariable("id") String id, @RequestBody Tcc tcc) {
         // Assume que o ID é passado como string, você pode alterar conforme necessário
         Tcc updatedTcc = tccService.updateTCC(tcc);
@@ -44,7 +46,7 @@ public class TccController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteTcc(@PathVariable("id") String id) {
         // Assume que o ID é passado como string, você pode alterar conforme necessário
         Tcc tccToDelete = new Tcc(); // Você precisa criar um objeto Tcc com o ID fornecido
@@ -56,13 +58,13 @@ public class TccController {
         }
     }
 
-    @GetMapping
+    @GetMapping("/list")
     public ResponseEntity<List<Tcc>> getAllTccs() {
         List<Tcc> tccs = tccService.getAllTCC();
         return new ResponseEntity<>(tccs, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/list/{id}")
     public ResponseEntity<Tcc> getTccById(@PathVariable("id") String id) {
         // Assume que o ID é passado como string, você pode alterar conforme necessário
         Tcc tcc = new Tcc(); // Você precisa criar um objeto Tcc com o ID fornecido
