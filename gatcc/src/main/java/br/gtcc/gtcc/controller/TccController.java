364b0fcfa4f1490f74fd5b91287149f5a287abcd
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-10 22:03:53 -0300

    Add bean CrossOrigin

--- gatcc/src/main/java/br/gtcc/gtcc/controller/TccController.java
+++ gatcc/src/main/java/br/gtcc/gtcc/controller/TccController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.gtcc.gtcc.model.nitriteid.Tcc;
 import br.gtcc.gtcc.services.impl.nitritedb.TccService;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/v1/tccs")
 public class TccController {
