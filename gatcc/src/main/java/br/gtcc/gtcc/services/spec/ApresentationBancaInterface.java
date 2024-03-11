9f0147aeeb4b2c576c1344f5a7ff3bf91cee1c68
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-10 21:48:29 -0300

    Add interface ApresentationBanca

---
+++ gatcc/src/main/java/br/gtcc/gtcc/services/spec/ApresentationBancaInterface.java
@@ -0,0 +1,20 @@
+package br.gtcc.gtcc.services.spec;
+
+import java.util.List;
+
+import br.gtcc.gtcc.model.nitriteid.ApresentationBanca;
+
+public interface ApresentationBancaInterface {
+ 
+    
+   public  ApresentationBanca createApresentationBanca ( ApresentationBanca apresentationBanca);
+   
+   public  ApresentationBanca updateApresentationBanca ( ApresentationBanca apresentationBanca);
+   
+   public  ApresentationBanca deleteApresentationBanca ( ApresentationBanca apresentationBanca);
+   
+   public List< ApresentationBanca> getAllApresentationBanca ();
+   
+   public  ApresentationBanca getApresentationBanca ( ApresentationBanca apresentationBanca);
+
+}
