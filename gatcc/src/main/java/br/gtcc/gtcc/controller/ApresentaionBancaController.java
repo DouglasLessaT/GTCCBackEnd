a8692f85e34ac3f755c84cd04ed20417b915d087
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-10 22:03:02 -0300

    Create controller entity ApresentationBanca

---
+++ gatcc/src/main/java/br/gtcc/gtcc/controller/ApresentaionBancaController.java
@@ -0,0 +1,12 @@
+package br.gtcc.gtcc.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/v1/apresentation_banca")
+public class ApresentaionBancaController {
+    
+}
