a3a400ae0af01c9242c0b3495cb6227ac2960d1f
MAGOCIZENTO <douglaslessat@gmail.com>
2024-03-18 21:16:30 -0300

    alterando Paramentro interface createApresentationBanca

--- gtcc/src/main/java/br/gtcc/gtcc/services/spec/ApresentationBancaInterface.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/spec/ApresentationBancaInterface.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import br.gtcc.gtcc.model.nitriteid.ApresentationBanca;
 
+
 public interface ApresentationBancaInterface {
  
     
-   public  ApresentationBanca createApresentationBanca ( ApresentationBanca apresentationBanca);
+   public  ApresentationBanca createApresentationBanca (ApresentationBanca apresentationBanca);
    
    public  ApresentationBanca updateApresentationBanca ( ApresentationBanca apresentationBanca);
    
