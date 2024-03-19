b26e903fbb523a9d718b1138dc29e7b0c01a58f5
MAGOCIZENTO <douglaslessat@gmail.com>
2024-03-18 21:21:59 -0300

    alteração class ApresentaionBancaController

--- gtcc/src/main/java/br/gtcc/gtcc/controller/ApresentaionBancaController.java
+++ gtcc/src/main/java/br/gtcc/gtcc/controller/ApresentaionBancaController.java
@@ -1,13 +1,12 @@
 package br.gtcc.gtcc.controller;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gtcc.gtcc.model.nitriteid.ApresentationBanca;
-import br.gtcc.gtcc.model.nitriteid.Users;
 import br.gtcc.gtcc.services.spec.ApresentationBancaInterface;
+
+
+import java.util.List;// pode usar para apresentaçao
+
 
 @CrossOrigin
 @RestController
