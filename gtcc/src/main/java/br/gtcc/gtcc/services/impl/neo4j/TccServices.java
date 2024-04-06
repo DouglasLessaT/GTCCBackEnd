9831a144bd1741c9165bbb780b215415f77bf3cb
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-06 10:55:03 -0300

    Adicionando esqueletos do TccInterface( Métodos)

--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/TccServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/TccServices.java
@@ -1,5 +1,42 @@
 package br.gtcc.gtcc.services.impl.neo4j;
 
-public class TccServices {
- 
+import br.gtcc.gtcc.services.spec.TccInterface;
+import br.gtcc.gtcc.model.neo4j.Tcc;
+import br.gtcc.gtcc.model.neo4j.repository.DataRepository;
+
+import java.util.List;
+
+public class TccServices implements TccInterface<Tcc, String>{
+
+    @Override
+    public Tcc createTcc(Tcc tcc) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Tcc deleteTCC(String id) { 
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Tcc> getAllTCC() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Tcc getTCC(String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Tcc updateTCC(Tcc tcc) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
 }
