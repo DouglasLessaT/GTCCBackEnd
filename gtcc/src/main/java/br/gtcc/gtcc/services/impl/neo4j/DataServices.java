7fc41742e36e27a0bc226ce7655e7d630662b9b1
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-06 11:30:18 -0300

    Adicionado metodos de retorno como null

--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/DataServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/DataServices.java
@@ -4,6 +4,8 @@ import br.gtcc.gtcc.services.spec.DataInterface;
 import br.gtcc.gtcc.model.neo4j.Data;
 import br.gtcc.gtcc.model.neo4j.repository.DataRepository;
 
+import java.util.List;
+
 
 
 public class DataServices implements DataInterface<Data, String>{
@@ -11,28 +13,28 @@ public class DataServices implements DataInterface<Data, String>{
    
    @Override
    public  Data   createData(Data data){
-
-   };
+       return null;
+   }
    
    @Override
    public  Data   updateData(Data Data){
-
-   };
+       return null;
+   }
    
    @Override
    public  Data   deleteData(String data){
-
-   };
+       return null;
+   }
    
    @Override
    public  Data   getData(String data){
-
-   };
+       return null;
+   }
    
    @Override
    public List< Data > getAllData(){
-    
-   };
+       return null;
+   }
 
     
 }
