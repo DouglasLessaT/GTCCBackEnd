3f422288d82060683d50b7a35d72ae5dfac8f661
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-06 11:26:26 -0300

    Troca de nome das classe

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/ApresentationBancaRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/ApresentationBancaRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import br.gtcc.gtcc.model.neo4j.ApresentationBanca;
 
-public interface ApresentationBancaServices extends Neo4jRepository<ApresentationBanca, String> {
+public interface ApresentationBancaRepository extends Neo4jRepository<ApresentationBanca, String> {
  
 }
