5de63e56808a0b15dbe29544d2745a5258d16b0f
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-06 11:29:58 -0300

    Adicionado metodos da interface dentro da classe ApresentationBanca

--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/ApresentationBancaServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/ApresentationBancaServices.java
@@ -1,5 +1,36 @@
 package br.gtcc.gtcc.services.impl.neo4j;
 
-public class ApresentationBancaServices {
+import br.gtcc.gtcc.services.spec.ApresentationBancaInterface;
+import br.gtcc.gtcc.model.neo4j.ApresentationBanca;
+import br.gtcc.gtcc.model.neo4j.repository.ApresentationBancaRepository;
+import java.util.List;
+
+
+public class ApresentationBancaServices implements ApresentationBancaInterface<ApresentationBanca, String>{
+
+    @Override
+    public ApresentationBanca createApresentationBanca(ApresentationBanca apresentationBanca) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ApresentationBanca updateApresentationBanca(ApresentationBanca apresentationBanca) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ApresentationBanca deleteApresentationBanca(String apresentationBanca) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ApresentationBanca getApresentationBanca(String apresentationBanca) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<ApresentationBanca> getAllApresentationBanca() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
  
 }
