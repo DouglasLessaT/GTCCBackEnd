28a01b841070d422fdcf14e1fdfcb27749b0bfbb
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-06 11:05:41 -0300

    Adicionado métodos dos de DataInterface

--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/DataServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/DataServices.java
@@ -1,5 +1,38 @@
 package br.gtcc.gtcc.services.impl.neo4j;
 
-public class DataServices {
- 
+import br.gtcc.gtcc.services.spec.DataInterface;
+import br.gtcc.gtcc.model.neo4j.Data;
+import br.gtcc.gtcc.model.neo4j.repository.DataRepository;
+
+
+
+public class DataServices implements DataInterface<Data, String>{
+
+   
+   @Override
+   public  Data   createData(Data data){
+
+   };
+   
+   @Override
+   public  Data   updateData(Data Data){
+
+   };
+   
+   @Override
+   public  Data   deleteData(String data){
+
+   };
+   
+   @Override
+   public  Data   getData(String data){
+
+   };
+   
+   @Override
+   public List< Data > getAllData(){
+    
+   };
+
+    
 }
