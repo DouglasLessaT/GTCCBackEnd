f5bc061a976e53f13f674e15d32ca37a2a80b59e
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-11 00:35:42 -0300

    Injetando dependencias dentro da services TccServices

--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/TccServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/TccServices.java
@@ -2,11 +2,16 @@ package br.gtcc.gtcc.services.impl.neo4j;
 
 import br.gtcc.gtcc.services.spec.TccInterface;
 import br.gtcc.gtcc.model.neo4j.Tcc;
-import br.gtcc.gtcc.model.neo4j.repository.DataRepository;
+import br.gtcc.gtcc.model.neo4j.repository.TccRepository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TccServices implements TccInterface<Tcc, String>{
+
+    @Autowired
+    public TccRepository repository;
 
     @Override
     public Tcc createTcc(Tcc tcc) {
