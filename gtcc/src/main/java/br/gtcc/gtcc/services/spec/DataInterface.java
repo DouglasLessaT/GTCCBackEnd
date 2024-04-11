615223f02349f68feb8186704fbf4a9f5978e369
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-11 00:25:16 -0300

    Removendo import inútil

--- gtcc/src/main/java/br/gtcc/gtcc/services/spec/DataInterface.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/spec/DataInterface.java
@@ -2,9 +2,6 @@ package br.gtcc.gtcc.services.spec;
 
 import java.util.List;
 
-
-import br.gtcc.gtcc.model.nitriteid.Data; 
-
 public interface DataInterface<T, E> {
 
    public  T   createData(T data);
