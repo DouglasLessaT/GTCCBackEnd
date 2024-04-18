d8ce98359609e4b241946e3175d14b6805225758
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-18 18:17:19 -0300

    Removendo as anotações e comentando o Bean do neo4j para o CQL

--- gtcc/src/main/java/br/gtcc/gtcc/config/DBConfig.java
+++ gtcc/src/main/java/br/gtcc/gtcc/config/DBConfig.java
@@ -22,18 +22,16 @@ import org.neo4j.cypherdsl.core.renderer.Dialect;
  * Classe de Configuração do banco de dados com o NitriteDB
  */
 
-@Order(1)
+@Order(0)
 @Configuration
-@EnableNeo4jRepositories
-@EnableReactiveNeo4jRepositories
 public class DBConfig {
     
     //neo4j DB
-    @Bean("neo4j")
-    org.neo4j.cypherdsl.core.renderer.Configuration cypherDslConfiguration() {
-        return org.neo4j.cypherdsl.core.renderer.Configuration
-        .newConfig()
-        .withDialect(Dialect.NEO4J_5).build();
-    }
+//    @Bean("neo4j");
+//    org.neo4j.cypherdsl.core.renderer.Configuration cypherDslConfiguration() {
+//        return org.neo4j.cypherdsl.core.renderer.Configuration
+//        .newConfig()
+//        .withDialect(Dialect.NEO4J_5).build();
+//    }
 
 }
