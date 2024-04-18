ff399f832ae96e3e644f5a735c39295c2a36c532
SammLopes <sam.v.p.lopes@gmail.com>
2024-04-18 18:15:54 -0300

    Adicionado duas novas anotações dos repositórios do neo4j e o Bean para o Cypher(CQL)

--- gtcc/src/main/java/br/gtcc/gtcc/GtccApplication.java
+++ gtcc/src/main/java/br/gtcc/gtcc/GtccApplication.java
@@ -1,15 +1,28 @@
 package br.gtcc.gtcc;
 
+import org.neo4j.cypherdsl.core.renderer.Dialect;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.repository.config.EnableReactiveNeo4jRepositories;
 
 @SpringBootApplication
-@EnableNeo4jRepositories
+@EnableAutoConfiguration
+@EnableNeo4jRepositories(basePackages="br.gtcc.gtcc")
 public class GtccApplication {
-	public static void main(String[] args) {
-		
-		SpringApplication.run(GtccApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GtccApplication.class, args);
+    }
+
+    @Bean("neo4j")
+    org.neo4j.cypherdsl.core.renderer.Configuration cypherDslConfiguration() {
+        return org.neo4j.cypherdsl.core.renderer.Configuration
+                .newConfig()
+                .withDialect(Dialect.NEO4J_5).build();
+    }
 
 }
