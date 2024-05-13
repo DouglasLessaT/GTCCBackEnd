d760dc11b2f38fce0d9d2ad7834e9cfec7c29ec5
MAGOCIZENTO <douglaslessat@gmail.com>
2024-05-13 16:04:34 -0300

    deploy user e authfilter

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/UsersRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/UsersRepository.java
@@ -9,4 +9,5 @@ import br.gtcc.gtcc.model.neo4j.Users;
 public interface UsersRepository extends Neo4jRepository<Users, String> {
  Users findByName(String name);
  Optional<Users> findById(String id);
+ Users findByEmail(String email);
 }
