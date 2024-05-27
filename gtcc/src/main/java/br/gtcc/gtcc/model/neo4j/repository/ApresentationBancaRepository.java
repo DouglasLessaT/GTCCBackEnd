e1d8424d05710e506cf6d8dcbfe771892ff1cbe0
SammLopes <sam.v.p.lopes@gmail.com>
2024-05-26 21:13:53 -0300

    ALterando querys dentro dos repositorys

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/ApresentationBancaRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/ApresentationBancaRepository.java
@@ -1,11 +1,18 @@
 package br.gtcc.gtcc.model.neo4j.repository;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.gtcc.gtcc.model.neo4j.ApresentationBanca;
 
 public interface ApresentationBancaRepository extends Neo4jRepository<ApresentationBanca, String> {
- Optional<ApresentationBanca> findById(String id);
+ 
+
+    @Query("MATCH (a:ApresentationBanca)-[:MEMBER_OF]->(u:Users) WHERE elementId(u) IN [$member1Id, $member2Id] AND date(a.date) = date($date) AND a.horasComeco = $horasComeco AND a.horasFim = $horasFim RETURN count(a)")
+    Integer countByMembersDateAndTime(@Param("member1Id") String member1Id, @Param("member2Id") String member2Id, @Param("date") LocalDateTime date, @Param("horasComeco") LocalTime horasComeco, @Param("horasFim") LocalTime horasFim);
+
 }
--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/DataRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/DataRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.repository.query.Param;
 import br.gtcc.gtcc.model.neo4j.Data;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public interface DataRepository extends Neo4jRepository<Data, String> {
 
     Data findByDate(LocalDateTime date);
  
-    @Query("MATCH (d:Data) WHERE date(d.date).day = $day AND date(d.date).month = $month AND date(d.date).year = $year RETURN count(d)")
-    Integer countByDayMonthYear(@Param("day") int day, @Param("month") int month, @Param("year") int year);
+    @Query("MATCH (d:Data) WHERE date(d.date).day = $day AND date(d.date).month = $month AND date(d.date).year = $year AND d.horasComeco = $horasComeco AND d.horasFim = $horasFim RETURN count(d)")
+    Integer countByDayMonthYearAndHours(@Param("day") int day, @Param("month") int month, @Param("year") int year, @Param("horasComeco") LocalTime horasComeco, @Param("horasFim") LocalTime horasFim);
     
 }
