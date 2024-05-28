ca3251fbf509c42616c5b4ea9e8f6aa41ace7b19
Samuel Lopes <sam.v.p.lopes@gmail.com>
2024-05-28 15:36:52 -0300

    Adicionando possieis queries de conflinto

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/ApresentationBancaRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/ApresentationBancaRepository.java
@@ -15,4 +15,27 @@ public interface ApresentationBancaRepository extends Neo4jRepository<Apresentat
     @Query("MATCH (a:ApresentationBanca)-[:MEMBER_OF]->(u:Users) WHERE elementId(u) IN [$member1Id, $member2Id] AND date(a.date) = date($date) AND a.horasComeco = $horasComeco AND a.horasFim = $horasFim RETURN count(a)")
     Integer countByMembersDateAndTime(@Param("member1Id") String member1Id, @Param("member2Id") String member2Id, @Param("date") LocalDateTime date, @Param("horasComeco") LocalTime horasComeco, @Param("horasFim") LocalTime horasFim);
 
+    
+    @Query("MATCH (ap1:ApresentationBanca)-[:MEMBER_ONE_OF|:MEMBER_TWO_OF]->(user:Users), " +
+           "(ap1)-[:TCC_APRESENTA_EM]->(data1:Data) " +
+           "MATCH (ap2:ApresentationBanca)-[:MEMBER_ONE_OF|:MEMBER_TWO_OF]->(user), " +
+           "(ap2)-[:TCC_APRESENTA_EM]->(data2:Data) " +
+           "WHERE ap1.id <> ap2.id " +
+           "AND data1.date = $date " +
+           "AND data1.horasComeco = $horasComeco " +
+           "AND data1.horasFim = $horasFim " +
+           "AND (user.id = $member1Id OR user.id = $member2Id) " +
+           "RETURN count(DISTINCT ap1)")
+    Integer countConflictingApresentations(String date, String horasComeco, String horasFim, String member1Id, String member2Id);
+
+    @Query("MATCH (ap:ApresentationBanca)-[:MEMBER_ONE_OF|:MEMBER_TWO_OF]->(user:Users), " +
+           "(ap)-[:ON_DATE]->(data:Data) " +
+           "WHERE (user.id = $member1 OR user.id = $member2) " +
+           "AND data.date = $date " +
+           "AND data.horasComeco = $horasComeco " +
+           "AND data.horasFim = $horasFim " +
+           "RETURN count(DISTINCT ap)")
+    Integer countConflictingApresentationsByData(String date, String horasComeco, String horasFim, String member1, String member2);
+
+
 }
