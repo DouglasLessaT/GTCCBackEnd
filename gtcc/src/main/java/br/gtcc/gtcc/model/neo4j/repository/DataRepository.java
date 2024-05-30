25a34bb551c36a6b48472e8e4bb83c7a61c9cd95
SammLopes <sam.v.p.lopes@gmail.com>
2024-05-30 00:28:49 -0300

    Adicionado quers dentr do repository

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/DataRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/DataRepository.java
@@ -16,4 +16,11 @@ public interface DataRepository extends Neo4jRepository<Data, String> {
     @Query("MATCH (d:Data) WHERE date(d.date).day = $day AND date(d.date).month = $month AND date(d.date).year = $year AND d.horasComeco = $horasComeco AND d.horasFim = $horasFim RETURN count(d)")
     Integer countByDayMonthYearAndHours(@Param("day") int day, @Param("month") int month, @Param("year") int year, @Param("horasComeco") LocalTime horasComeco, @Param("horasFim") LocalTime horasFim);
     
+    @Query("match(a:Data)"+
+    "where a.data='2024-05-04'"+
+    "and ((time(a.hi)>time('11:50') and time(a.hi)<time('12:20'))"+
+    "or (time(a.hf)>time('11:50') and time(a.hf)<time('12:20'))"+
+    "or (time(a.hi)<time('11:50') and time(a.hf)>time('12:20'))"+
+    "or (time(a.hi)>time('11:50') and time(a.hf)<time('12:20'))) return a)")
+     Integer _countByDayMonthYearAndHours(@Param("day") int day, @Param("month") int month, @Param("year") int year, @Param("horasComeco") LocalTime horasComeco, @Param("horasFim") LocalTime horasFim);
 }
