f59db6877b6b3d4242dd2cff613ba32a4328a65d
SammLopes <sam.v.p.lopes@gmail.com>
2024-06-09 02:38:52 -0300

    Consertando a atualização de relacionamento da entidade Tcc

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/Users.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/Users.java
@@ -11,18 +11,22 @@ import org.springframework.data.neo4j.core.schema.Relationship.Direction;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.micrometer.common.lang.NonNull;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Node
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+
 public class Users {
 
     @Id
@@ -46,5 +50,18 @@ public class Users {
     
     @Relationship(type = "GERENCIA", direction = Relationship.Direction.OUTGOING)  
     private Set<Tcc> tccsGerenciados = new HashSet<>();
+
+    // @Override
+    // public String toString() {
+    //     return "Users{" +
+    //     "id='" + id + '\'' +
+    //     ", name='" + name + '\'' +
+    //     ", email='" + email + '\'' +
+    //     ", userType=" + userType +
+    //     ", birthdate=" + birthdate +
+    //     ", cellphone='" + cellphone + '\'' +
+    //     ", tccsGerenciados=" + tccsGerenciados +
+    //     '}';
+    // }
 
 }
--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/TccServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/TccServices.java
@@ -1,10 +1,14 @@
 package br.gtcc.gtcc.services.impl.neo4j;
 
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.neo4j.cypherdsl.core.Use;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.gtcc.gtcc.model.UserType;
 import br.gtcc.gtcc.model.neo4j.Tcc;
@@ -12,6 +16,7 @@ import br.gtcc.gtcc.model.neo4j.Users;
 import br.gtcc.gtcc.model.neo4j.repository.TccRepository;
 import br.gtcc.gtcc.model.neo4j.repository.UsersRepository;
 import br.gtcc.gtcc.services.spec.TccInterface;
+import br.gtcc.gtcc.util.Console;
 
 @Service
 public class TccServices implements TccInterface<Tcc, String> {
@@ -92,53 +97,56 @@ public class TccServices implements TccInterface<Tcc, String> {
                 existingTcc.setCurse(tcc.getCurse());
                 existingTcc.setDateOfApresentation(tcc.getDateOfApresentation());
 
-                if(tcc.getAluno() != null && tcc.getOrientador() != null){
+                if(tcc.getIdAluno() != null && tcc.getIdOrientador() != null){
 
-                    if(tcc.getIdAluno() != null && tcc.getIdOrientador() != null){
+                    Users orientador = this.usersRepository.findById(tcc.getIdOrientador()).get();
+                    Users aluno = this.usersRepository.findById(tcc.getIdAluno()).get();
 
-                        Users orientador = this.usersRepository.findById(tcc.getIdOrientador()).get();
-                        Users aluno = this.usersRepository.findById(tcc.getIdAluno()).get();
+                    if( orientador != null && aluno != null){
 
-                        if( orientador != null && aluno != null){
+                        EnumSet<UserType> userTypeCoordenador = EnumSet.of(UserType.COORDENADOR);
+                        EnumSet<UserType> userTypeProfessor = EnumSet.of(UserType.PROFESSOR);
+                        
+                        boolean isCoordenador = orientador.getUserType().equals(userTypeCoordenador);
+                        boolean isProfessor = orientador.getUserType().equals(userTypeProfessor);
+         
+                        if(isCoordenador == true || isProfessor == true){
 
-                            EnumSet<UserType> userTypeCoordenador = EnumSet.of(UserType.COORDENADOR);
-                            EnumSet<UserType> userTypeProfessor = EnumSet.of(UserType.PROFESSOR);
-                            
-                            boolean isCoordenador = orientador.getUserType().equals(userTypeCoordenador);
-                            boolean isProfessor = orientador.getUserType().equals(userTypeProfessor);
-            
-                            if(isCoordenador == true || isProfessor == true){
+                            Users orientadorRepo = existingTcc.getOrientador();
+                            Users alunoRepo = existingTcc.getAluno();
+    
+                            removeRelacionamento(orientadorRepo.getId(), id);
+                            removeRelacionamento(alunoRepo.getId(), id);
 
-                                orientador.getTccsGerenciados().add(existingTcc);
-                                aluno.getTccsGerenciados().add(existingTcc);
-                                existingTcc.setIdOrientador(tcc.getIdOrientador());
-                                existingTcc.setIdAluno(tcc.getIdAluno());
-                                existingTcc.setAluno(tcc.getAluno());
-                                existingTcc.setOrientador(tcc.getOrientador());
+                            existingTcc.setIdOrientador(tcc.getIdOrientador());
+                            existingTcc.setIdAluno(tcc.getIdAluno());
+                            existingTcc.setAluno(aluno);
+                            existingTcc.setOrientador(orientador);
 
-                                this.usersRepository.save(orientador);
-                                this.usersRepository.save(aluno);
+                            orientador.getTccsGerenciados().add(existingTcc);
+                            this.usersRepository.save(orientador);
+                            aluno.getTccsGerenciados().add(existingTcc);
+                            this.usersRepository.save(aluno);
 
-                            } else {
-
-                                return null;
-                            
-                            }
 
                         } else {
-                            
+
                             return null;
-                       
+                        
                         }
 
                     } else {
-
+                        
                         return null;
-
-                    }
                     
+                    }
+
+                } else {
+
+                    return null;
+
                 }
-                
+
                 return tccRepository.save(existingTcc);
 
             } else {
@@ -151,6 +159,10 @@ public class TccServices implements TccInterface<Tcc, String> {
             return null;
 
         }
+    }
+    
+    public void removeRelacionamento(String idUsuario, String idTcc) {
+        this.tccRepository.removeRelacaoEntreUsuarioTcc(idUsuario, idTcc);
     }
 
     @SuppressWarnings("unused")
