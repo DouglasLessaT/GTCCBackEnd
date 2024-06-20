9dac4a38e1413f7b3b31d974ebb71a56bc8f8e25
SammLopes <sam.v.p.lope@gmail.com>
2024-06-20 20:03:00 -0300

    Adiionando authorize no swagger

--- gtcc/src/main/java/br/gtcc/gtcc/config/DocConfig.java
+++ gtcc/src/main/java/br/gtcc/gtcc/config/DocConfig.java
@@ -5,9 +5,13 @@
 package br.gtcc.gtcc.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -22,13 +26,21 @@ import org.springframework.core.annotation.Order;
 @Order(2)
 public class DocConfig {
     @Bean
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI().info(new Info()
-                .title("API em NoSQL Neo4j GTCC")
-                .version("0.0.1")
-                .contact(new Contact().email("vix.bay.group@gmail.com")
-                .name("Arthur, Alessandro Santos, Hugo Alves, Douglas Lessa, Pedro Pompermayer, Samuel Paviotti"))
-                .description("<h1>API GTCC</h1><p>Repositório ainda não esta disponível</p>"
-                ));
-    }
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .addSecurityItem(new SecurityRequirement().addList("Auth JWT"))
+                                .components(new Components()
+                                                .addSecuritySchemes("Auth JWT",
+                                                                new SecurityScheme()
+                                                                                .name("Auth JWT")
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("Bearer")
+                                                                                .bearerFormat("JWT")))
+                                .info(new Info()
+                                                .title("API em NoSQL Neo4j GTCC")
+                                                .version("0.0.1")
+                                                .contact(new Contact().email("vix.bay.group@gmail.com")
+                                                                .name("Arthur, Alessandro Santos, Hugo da Cruz, Douglas Lessa, Pedro Pompermayer, Samuel Paviotti"))
+                                                .description("<h1>API GTCC</h1><p>Repositório ainda não esta disponível</p>"));
+        }
 }
