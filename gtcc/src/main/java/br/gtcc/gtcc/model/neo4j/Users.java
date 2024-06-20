ba89de18f1b7dc9db24310b8845582710dc09d4b
SammLopes <sam.v.p.lopes@gmail.com>
2024-06-20 18:33:50 -0300

    Alterando a model de usuários e adicionando buscas no repository

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/Users.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/Users.java
@@ -1,6 +1,8 @@
 package br.gtcc.gtcc.model.neo4j;
 
 import br.gtcc.gtcc.model.UserType;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
@@ -11,6 +13,7 @@ import org.springframework.data.neo4j.core.schema.Relationship.Direction;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.micrometer.common.lang.NonNull;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import lombok.AllArgsConstructor;
@@ -48,20 +51,21 @@ public class Users {
     @NonNull
     private String cellphone;                          
     
+    @NonNull
+    private String login;
+
+    @NonNull
+    private String senha;
+
+    private List<String> permissoes = new ArrayList<>();
+
     @Relationship(type = "GERENCIA", direction = Relationship.Direction.OUTGOING)  
     private Set<Tcc> tccsGerenciados = new HashSet<>();
 
-    // @Override
-    // public String toString() {
-    //     return "Users{" +
-    //     "id='" + id + '\'' +
-    //     ", name='" + name + '\'' +
-    //     ", email='" + email + '\'' +
-    //     ", userType=" + userType +
-    //     ", birthdate=" + birthdate +
-    //     ", cellphone='" + cellphone + '\'' +
-    //     ", tccsGerenciados=" + tccsGerenciados +
-    //     '}';
-    // }
+    public Users orElseThrow(Object object) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'orElseThrow'");
+    }
+    
 
 }
--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/UsersRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/UsersRepository.java
@@ -1,12 +1,30 @@
 package br.gtcc.gtcc.model.neo4j.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import br.gtcc.gtcc.model.neo4j.Users;
 
 public interface UsersRepository extends Neo4jRepository<Users, String> {
- Users findByName(String name);
+  
  Optional<Users> findById(String id);
+
+ @Query("MATCH (u:Users {login: $login}) RETURN u")
+ Optional<Users> findByLogin(String login);
+
+ List<Users> findAllByLoginOrderByLogin(String login);
+
+ List<Users> findAllByOrderByLoginAsc();
+
+ @Query("MATCH (u:Users {email: $0}) RETURN u")
+ Users findByEmail(String email);
+
+ @Query("MATCH (u:Users) WHERE 'ALUNO' IN u.userType RETURN u")
+ List<Users> findAlunos();
+
+ @Query("MATCH (u:Users) WHERE 'PROFESSOR' IN u.userType RETURN u ")
+ List<Users> findProfessores();
 }
