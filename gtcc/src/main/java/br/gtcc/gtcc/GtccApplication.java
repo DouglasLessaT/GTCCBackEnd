c36fbe27e5f3b6d7e16dc5bf14ad3dd4face9c23
SammLopes <sam.v.p.lopes@gmail.com>
2024-06-20 18:31:51 -0300

    Desabilitando tela de login do swagger e adicionando arquivo propriedaes do JWT e o pacote util do JWT

--- gtcc/src/main/java/br/gtcc/gtcc/GtccApplication.java
+++ gtcc/src/main/java/br/gtcc/gtcc/GtccApplication.java
@@ -4,12 +4,13 @@ import org.neo4j.cypherdsl.core.renderer.Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.repository.config.EnableReactiveNeo4jRepositories;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) // desativa a tela de login no inicio
+//@EnableAutoConfiguration
 @EnableNeo4jRepositories(basePackages="br.gtcc.gtcc")
 public class GtccApplication {
 
