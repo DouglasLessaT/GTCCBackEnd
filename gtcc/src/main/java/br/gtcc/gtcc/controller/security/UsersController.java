6d254a29e874234a41f68343da7f14248dfe36e4
SammLopes <sam.v.p.lopes@gmail.com>
2024-06-20 18:33:20 -0300

    Adicionando rotas de profesor e usuários

---
+++ gtcc/src/main/java/br/gtcc/gtcc/controller/security/UsersController.java
@@ -0,0 +1,136 @@
+package br.gtcc.gtcc.controller.security;
+
+import br.gtcc.gtcc.annotations.ValidaAcesso;
+import br.gtcc.gtcc.model.neo4j.Users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import br.gtcc.gtcc.services.spec.UserInterface;
+import br.gtcc.gtcc.util.Console;
+
+import java.util.Optional;
+
+/**
+ *
+ * @author mrbee
+ * 
+ * Controller da entidade Users, criação de rotas  
+ */
+
+@CrossOrigin("*")
+@RestController
+@ValidaAcesso("ROLE_ADMIN")
+@RequestMapping("coordenacao/tcc/v1")
+public class UsersController {
+    
+    @SuppressWarnings("rawtypes")
+    @Autowired
+    public UserInterface usersInterface;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+        
+    @PostMapping("/usuario")
+    public ResponseEntity<Object> createUser(@RequestBody(required = true) Users users){
+        users.setSenha(passwordEncoder.encode(users.getSenha()));
+        @SuppressWarnings("unchecked")
+        Optional<Users> createdUsers = Optional.ofNullable((Users) usersInterface.createUsers(users));
+        
+        if (createdUsers.isPresent()) {
+        
+            return ResponseEntity.status(HttpStatus.CREATED).body("Usuário criado com sucesso");
+        
+        } else {
+        
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado"); 
+        
+        } 
+    }
+
+    @DeleteMapping("/usuario/{id}")
+    public ResponseEntity<Object> deleteUsers(@PathVariable String id){
+    
+        @SuppressWarnings("unchecked")
+        Optional<Users> deletedUsers = Optional.ofNullable((Users) usersInterface.deleteUsers(id));  
+
+        if (deletedUsers.isPresent()) {
+
+            return ResponseEntity.status(HttpStatus.CREATED).body("Usuário deletado com sucesso");
+        
+        } else {
+        
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuáio não encontrado");
+        
+        } 
+
+    }
+    
+    @PutMapping("/usuario/{id}")
+    public ResponseEntity<Object> updateUsers(@RequestBody(required = true) Users users, @PathVariable("id") String id){
+        
+        @SuppressWarnings("unchecked")
+        Optional<Users> updatedUser = Optional.ofNullable((Users)  usersInterface.updateUsers(users , id));
+
+        if (updatedUser.isPresent()) {
+        
+            return ResponseEntity.status(HttpStatus.OK).body("Usuario alterado com sucesso");
+        
+        } else {
+        
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        
+        } 
+    
+    }
+    
+    @GetMapping("/usuarios")
+    public ResponseEntity<Object> getAllUsers(){
+        
+       @SuppressWarnings("unchecked")
+       Optional<List<Users>> list = Optional.ofNullable((List<Users>) usersInterface.getAllUsers());
+       
+       if(list.isPresent()){
+    
+            return new ResponseEntity<>( list , HttpStatus.FOUND);
+           
+       }else {
+       
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuários não encontrados");
+       }
+       
+    }
+    
+    @GetMapping("/usuario/{id}")
+    public ResponseEntity<Object> getUser(@PathVariable String id){
+        
+        @SuppressWarnings("unchecked")
+        Optional<Users> foundUsers = Optional.ofNullable( (Users) usersInterface.getUsers(id));
+        
+
+        if (foundUsers.isPresent()) {
+        
+            return new ResponseEntity<>(foundUsers, HttpStatus.FOUND);
+        
+        } else {
+        
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        
+        }
+ 
+
+    }
+    
+}
