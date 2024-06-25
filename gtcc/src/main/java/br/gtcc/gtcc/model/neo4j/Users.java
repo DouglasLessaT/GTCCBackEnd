4a9c5151861caa9d6deec10aa226b24d226c1806
ArtthurRibbeiro <art.rib@hotmail.com>
2024-06-24 23:54:19 -0300

    conflitos de imports

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/Users.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/Users.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import br.gtcc.gtcc.model.UserType;
+
 
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
@@ -13,16 +15,26 @@ import org.springframework.data.neo4j.core.schema.Relationship;
 
 // import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import br.gtcc.gtcc.model.UserType;
 import io.micrometer.common.lang.NonNull;
+import org.springframework.data.neo4j.core.schema.Relationship.Direction;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.micrometer.common.lang.NonNull;
+import java.util.Objects;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Node
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+
 public class Users {
 
     @Id
@@ -52,10 +64,14 @@ public class Users {
 
     private List<String> permissoes = new ArrayList<>();
 
-    @NonNull
     @Relationship(type = "GERENCIA", direction = Relationship.Direction.OUTGOING)  
     private Set<Tcc> tccsGerenciados = new HashSet<>();
 
+    public Users orElseThrow(Object object) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'orElseThrow'");
+    }
+    
 
 
 
