689772a406dfa28b502ba396cff12a3456b584af
MAGOCIZENTO <douglaslessat@gmail.com>
2024-06-25 12:16:09 -0300

    CONSERTANDO COODENADORCONTROLLER

--- gtcc/src/main/java/br/gtcc/gtcc/controller/security/CoordenadorController.java
+++ gtcc/src/main/java/br/gtcc/gtcc/controller/security/CoordenadorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gtcc.gtcc.annotations.ValidaAcesso;
@@ -19,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @CrossOrigin
 @ValidaAcesso("ROLE_COORDENADOR")
@@ -48,7 +52,7 @@ public class CoordenadorController {
         }
     }
 
-    @GetMapping("/Professores")
+    @GetMapping("/professores")
     public ResponseEntity<Object> getAllProfessores() {
         Optional<List<Users>> list = Optional.ofNullable(usersInterface.getProfessores());
         if (list.isPresent()) {
@@ -58,4 +62,52 @@ public class CoordenadorController {
         }
     }
 
+        @PostMapping("/professor")
+    public ResponseEntity<Object> createProfessor(@RequestBody Users users) {
+        try {
+            Users createdUser = usersInterface.createdProfessor(users);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao criar o usuário");
+        }
+    }
+
+        @DeleteMapping("/professor/{id}")
+    public ResponseEntity<Object> deleteProfessor(@PathVariable String id){
+    
+        @SuppressWarnings("unchecked")
+        Optional<Users> deletedUsers = Optional.ofNullable((Users) usersInterface.deleteProfessor(id));  
+
+        if (deletedUsers.isPresent()) {
+
+            return ResponseEntity.status(HttpStatus.OK).body("Usuário deletado com sucesso");
+        
+        } else {
+        
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuáio não encontrado");
+        
+        } 
+
+    }
+    
+    @PutMapping("/professor/{id}")
+    public ResponseEntity<Object> updateProfessor(@RequestBody(required = true) Users users ,@PathVariable("id") String id){
+        
+        @SuppressWarnings("unchecked")
+        Optional<Users> updatedUser = Optional.ofNullable((Users)  usersInterface.updateProfessor(users , id));
+
+        if (updatedUser.isPresent()) {
+        
+            return ResponseEntity.status(HttpStatus.OK).body("Usuario alterado com sucesso");
+        
+        } else {
+        
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        
+        } 
+    
+    }
+    
 }
