9fda40dc5a379936a0c599566f6a413fe145e67c
MAGOCIZENTO <douglaslessat@gmail.com>
2024-06-30 03:06:36 -0300

    ApresentationBancaController

--- gtcc/src/main/java/br/gtcc/gtcc/controller/ApresentationBancaController.java
+++ gtcc/src/main/java/br/gtcc/gtcc/controller/ApresentationBancaController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gtcc.gtcc.annotations.ValidaAcesso;
+import br.gtcc.gtcc.model.neo4j.Agenda;
 import br.gtcc.gtcc.model.neo4j.ApresentationBanca;
+import br.gtcc.gtcc.services.spec.AgendaInterface;
 import br.gtcc.gtcc.services.spec.ApresentationBancaInterface;
 import br.gtcc.gtcc.util.Console;
 
@@ -28,32 +30,38 @@ import java.util.Optional;
 @ValidaAcesso("ROLE_PROFESSOR")
 @RequestMapping("coordenacao/tcc/v1")
 public class ApresentationBancaController {
-    
- @SuppressWarnings("rawtypes")
-@Autowired
- private  ApresentationBancaInterface interfaceBanca;
 
- @PostMapping("/apresentacao")
-   public ResponseEntity<Object> createApresentantion(@RequestBody ApresentationBanca apresentation) {
-       
-       @SuppressWarnings("unchecked")
-       Optional<ApresentationBanca>  createdApresentationBanca  = Optional.ofNullable((ApresentationBanca)interfaceBanca.createApresentationBanca(apresentation));
-      
-       if (  createdApresentationBanca.isPresent()) { 
-           
-           return ResponseEntity.status(HttpStatus.CREATED).body("Apresentação marcada com sucesso");
-       
-       } else {
-       
-           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Apresentação não marcada");
-       
-       }
-   }
+    @SuppressWarnings("rawtypes")
+    @Autowired
+    private AgendaInterface interfaceAgenda;
 
-   @DeleteMapping("/apresentacao/{id}")
-    public ResponseEntity<Object> deleteApresentationBanca(@PathVariable("id") String id){
-    
-        Optional<ApresentationBanca> deletedApresentationBanca = Optional.ofNullable((ApresentationBanca)interfaceBanca.deleteApresentationBanca(id));  
+    @SuppressWarnings("rawtypes")
+    @Autowired
+    private ApresentationBancaInterface interfaceBanca;
+
+    @PostMapping("/apresentacao")
+    public ResponseEntity<Object> createApresentantion(@RequestBody ApresentationBanca apresentation) {
+
+        @SuppressWarnings("unchecked")
+        Optional<ApresentationBanca> createdApresentationBanca = Optional
+                .ofNullable((ApresentationBanca) interfaceBanca.createApresentationBanca(apresentation));
+
+        if (createdApresentationBanca.isPresent()) {
+
+            return ResponseEntity.status(HttpStatus.CREATED).body("Apresentação marcada com sucesso");
+
+        } else {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Apresentação não marcada");
+
+        }
+    }
+
+    @DeleteMapping("/apresentacao/{id}")
+    public ResponseEntity<Object> deleteApresentationBanca(@PathVariable("id") String id) {
+
+        Optional<ApresentationBanca> deletedApresentationBanca = Optional
+                .ofNullable((ApresentationBanca) interfaceBanca.deleteApresentationBanca(id));
 
         if (deletedApresentationBanca.isPresent()) {
 
@@ -68,9 +76,11 @@ public class ApresentationBancaController {
     }
 
     @PutMapping("/apresentacao/{id}")
-    public ResponseEntity<Object> updateApresentationBanca(@PathVariable("id") String id ,@RequestBody(required = true) ApresentationBanca apresentation){ 
- 
-       Optional<ApresentationBanca> updatedApresentationBanca =  Optional.ofNullable((ApresentationBanca)interfaceBanca.updateApresentationBanca(id,apresentation));
+    public ResponseEntity<Object> updateApresentationBanca(@PathVariable("id") String id,
+            @RequestBody(required = true) ApresentationBanca apresentation) {
+
+        Optional<ApresentationBanca> updatedApresentationBanca = Optional
+                .ofNullable((ApresentationBanca) interfaceBanca.updateApresentationBanca(id, apresentation));
 
         if (updatedApresentationBanca.isPresent()) {
 
@@ -85,10 +95,11 @@ public class ApresentationBancaController {
     }
 
     @GetMapping("/apresentacao/{id}")
-    public ResponseEntity<Object> getApresentationBancaById(@PathVariable("id") String id ){ 
- 
+    public ResponseEntity<Object> getApresentationBancaById(@PathVariable("id") String id) {
+
         @SuppressWarnings("unchecked")
-        Optional<ApresentationBanca> getApresentationBanca =  Optional.ofNullable((ApresentationBanca) interfaceBanca.getApresentationBanca(id));
+        Optional<ApresentationBanca> getApresentationBanca = Optional
+                .ofNullable((ApresentationBanca) interfaceBanca.getApresentationBanca(id));
 
         if (getApresentationBanca.isPresent()) {
 
@@ -103,17 +114,36 @@ public class ApresentationBancaController {
     }
 
     @GetMapping("/apresentacoes")
-    public ResponseEntity<Object> getAllApresentationBanca(){ 
- 
-        Optional<List<ApresentationBanca>> getApresentationBancaList = Optional.ofNullable((List<ApresentationBanca>) interfaceBanca.getAllApresentationBanca());
-        
+    public ResponseEntity<Object> getAllApresentationBanca() {
+
+        Optional<List<ApresentationBanca>> getApresentationBancaList = Optional
+                .ofNullable((List<ApresentationBanca>) interfaceBanca.getAllApresentationBanca());
+
         if (getApresentationBancaList.isPresent()) {
-        
-            return new ResponseEntity<>( getApresentationBancaList, HttpStatus.OK);
-        
+
+            return new ResponseEntity<>(getApresentationBancaList, HttpStatus.OK);
+
         } else {
 
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @GetMapping("/agendasLivres")
+    public ResponseEntity<Object> getAllAgendas() {
+
+        List<Optional<Agenda>> agendas = (List<Optional<Agenda>>) interfaceAgenda.getAllAgenda();
+
+        if (agendas.isEmpty() != true) {
+
+            return new ResponseEntity<>(agendas, HttpStatus.OK);
+
+        } else {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Sem datas cadatradas");
 
         }
 
