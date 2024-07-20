80f897d367c90a68e42e365ac38d9af0831014aa
SammLopes <sam.v.p.lopes@gmail.com>
2024-07-20 00:50:32 -0300

    Adicionado novos métodos para manipulação do banco

--- gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/TccRepository.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/neo4j/repository/TccRepository.java
@@ -8,16 +8,29 @@ import org.springframework.data.repository.query.Param;
 
 import br.gtcc.gtcc.model.neo4j.Tcc;
 import br.gtcc.gtcc.model.neo4j.Users;
+import jakarta.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
 public interface TccRepository extends Neo4jRepository<Tcc, String> {
- Tcc findByTitle(String title); // implementação futura
- Optional<Tcc> findById(String id);
 
+    Optional<Tcc> findById(String id);
+
+    @Query("MATCH(t:Tcc) WHERE t.title CONTAINS $title RETURN t LIMIT 10")
+    Tcc buscarTccPorTitulo(@Param("title") String title); 
+
+    @Transactional
     @Query("MATCH(u:Users)-[r:GERENCIA]->(t:Tcc)WHERE elementId(u)=$idUsuario AND elementId(t)=$idTcc DETACH DELETE r ")
     Void removeRelacaoEntreUsuarioTcc(@Param("idUsuario") String idUsuario ,@Param("idTcc") String idTcc );
+
+    @Transactional
+    @Query("MATCH(u:Users)-[r:ORIENTA]->(t:Tcc)WHERE elementId(u)=$idUsuario AND elementId(t)=$idTcc DETACH DELETE r ")
+    Void removeRelacaoOrientaEntreUsuarioTcc(@Param("idUsuario") String idUsuario ,@Param("idTcc") String idTcc );
+
+    @Transactional
+    @Query("MATCH(u:Users)-[r:REALIZA]->(t:Tcc)WHERE elementId(u)=$idUsuario AND elementId(t)=$idTcc DETACH DELETE r ")
+    Void removeRelacaoRealizaaEntreUsuarioTcc(@Param("idUsuario") String idUsuario ,@Param("idTcc") String idTcc );
 
     @Query("MATCH(u:Users)WHERE elementId(u)=$idElement RETURN u")
     Users getUsers(@Param("idElement") String idElement);
