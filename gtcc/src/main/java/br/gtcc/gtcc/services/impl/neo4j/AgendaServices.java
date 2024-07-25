2c8646a1bdda5aadb4e71c5e95ceba465dd38fa4
SammLopes <sam.v.p.lopes@gmail.com>
2024-07-24 22:25:14 -0300

    Update da agenda service injetando dependencias de AgendaUtil e alterando todos os métodos da classe

--- gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/AgendaServices.java
+++ gtcc/src/main/java/br/gtcc/gtcc/services/impl/neo4j/AgendaServices.java
@@ -1,13 +1,14 @@
 package br.gtcc.gtcc.services.impl.neo4j;
 
 import br.gtcc.gtcc.services.spec.AgendaInterface;
-import br.gtcc.gtcc.util.Console;
+import br.gtcc.gtcc.util.services.AgendaUtil;
+import br.gtcc.gtcc.util.services.ApresentacaoUtil;
 import br.gtcc.gtcc.model.neo4j.Agenda;
-
-import br.gtcc.gtcc.model.neo4j.repository.AgendaRepository;
-
+import br.gtcc.gtcc.model.neo4j.ApresentationBanca;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import br.gtcc.gtcc.util.Console;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,133 +17,86 @@ import org.springframework.stereotype.Service;
 public class AgendaServices implements AgendaInterface<Agenda, String>{
 
    @Autowired
-   public AgendaRepository repository;
+   public AgendaUtil agendaUtil;
+
+   @Autowired
+   public ApresentacaoUtil aprUtil;
    
    @Override
-   public  Agenda   createAgenda(Agenda data){
+   public Agenda createAgenda(Agenda data){
        
-       if(data.getDate() != null){
+        this.agendaUtil.validaIdAgendaParaCriacao(data.getId());
     
+        LocalDateTime agenda = data.getDate();
         LocalTime horasComeco = data.getHorasComeco();
         LocalTime horasFim = data.getHorasFim();
+        this.agendaUtil.validaData(agenda);     
+        this.agendaUtil.validaHoras(horasComeco, horasFim);
         
-        Integer countAgenda =  repository.countByDateAndHours( data.getDate() ,horasComeco ,horasFim );
+        this.agendaUtil.countConflicts(agenda, horasComeco, horasFim);
 
-        Boolean exists = countAgenda > 0;
-
-        if (exists) {
-
-            throw new IllegalArgumentException("A data fornecida ja esta acriada.");
-        
-        }
-        
-        return this.repository.save(data);
-
-       }
-       
-        throw new IllegalArgumentException("A data fornecida é nula.");
+        return this.agendaUtil.salvarAgenda(data);
    
    }
    
    @Override
-   public   Agenda    updateAgenda (String id , Agenda   agenda ){
+   public Agenda updateAgenda (String id ,Agenda agenda ){
 
-        if(id != null &&  agenda  != null){
+        LocalDateTime date = agenda.getDate();
+        LocalTime horasComeco = agenda.getHorasComeco();
+        LocalTime horasFim = agenda.getHorasFim();
+        this.agendaUtil.validaId(id);
+        this.agendaUtil.validaData(date);     
+        this.agendaUtil.validaHoras(horasComeco, horasFim);
 
-            Agenda  dataRepo = this.getAgenda(id);
+        this.agendaUtil.checkExistAgenda(id);
+        Agenda dataRepo = this.agendaUtil.buscarAgenda(id);
 
-            if( dataRepo != null){
+        this.agendaUtil.countConflicts(date, horasComeco, horasFim);
+        dataRepo = this.agendaUtil.moldAgenda(dataRepo, agenda);
 
-                Boolean exists = repository.countByDateAndHours( agenda.getDate() , agenda.getHorasComeco() , agenda.getHorasFim() ) > 0;
-
-                if (exists) {
-
-                    throw new IllegalArgumentException("A data fornecida ja esta acriada.");
-                        
-                }
-
-                if(agenda.getApresentacao() != null){
-
-                    dataRepo.setApresentacao( agenda.getApresentacao());
-                
-                }
-                dataRepo.setDate( agenda.getDate());     
-                dataRepo.setHorasComeco( agenda.getHorasComeco());
-                dataRepo.setHorasFim( agenda.getHorasFim());    
-                dataRepo.setIsLock( agenda.getIsLock());       
-                return this.repository.save(dataRepo);
-
-            } else {
-                
-                throw new IllegalArgumentException("A data para atualizar é nula ou não existe.");
-        
-            }
-
-        }
-   
-        throw new IllegalArgumentException("A data fornecida é nula.");
+        return this.agendaUtil.salvarAgenda(dataRepo);
    
    }
    
    @Override
-   public   Agenda    deleteAgenda(String id){
+   public Agenda deleteAgenda(String id){
 
-        if(id != null){
-
-            Agenda  dataRepo = this.repository.existsById(id)==true? repository.findById(id).get() : null;
-
-            if(dataRepo != null){
-
-                this.repository.deleteById(id);
+        this.agendaUtil.validaId(id);
+        this.agendaUtil.checkExistAgenda(id);
+        Agenda dataRepo = this.agendaUtil.buscarAgenda(id);
+        this.agendaUtil.deletarAgenda(id);
                 return dataRepo;
 
-            } else {
-
-                throw new IllegalArgumentException("A data fornecida não existe.");
-            }
-
-        }
-
-        throw new IllegalArgumentException("A data fornecida é nula.");
     }
    
-   @SuppressWarnings("unused")
     @Override
-    public   Agenda    getAgenda(String id){
+    public Agenda getAgenda(String id){
 
-        if(id != null || id != " "){
-
-            return this.repository.existsById(id)==true? repository.findById(id).get() : null;
-
-        }
-
-        return null;
+        this.agendaUtil.validaId(id);
+        this.agendaUtil.checkExistAgenda(id);
+        return this.agendaUtil.buscarAgenda(id);
 
     }
    
    @Override
-   public List<  Agenda  > getAllAgenda(){
+   public List<Agenda> getAllAgenda(){
     
-    Long countAgendas = this.repository.count();
-    
-    if(countAgendas > 0){
-
-        return this.repository.findAll();
-
-    }
-
-    return null;
+        this.agendaUtil.countAgendas();
+        return this.agendaUtil.buscarTodasAgendas();
    
    }
 
    @Override
-   public List<  Agenda  > getAllAgendasFree(){
+   public List<Agenda> getAllAgendasFree(){
     
-    Long countAgendas = this.repository.count();
-    
-    if(countAgendas > 0){
+        this.agendaUtil.countAgendasFree();
+        return this.agendaUtil.buscarTodasAgendasLivres();
+   
+  }
 
-        return this.repository.listAgendaFree();
+  @Override
+  public Agenda adicionarApresentacaoEemAgenda(String idApresentacao , String idAgenda){
 
     }
 
