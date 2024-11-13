c620693556032eaeebfdc076db54ceb314e442c9
Douglas <douglaslessat@gmail.com>
2024-11-13 10:36:00 -0300

    cocertando grupo e convite

--- gtcc/src/main/java/br/gtcc/gtcc/controller/GrupoController.java
+++ gtcc/src/main/java/br/gtcc/gtcc/controller/GrupoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("coordenacao/tcc/v1/convites")
+@RequestMapping("coordenacao/tcc/v1/grupos")
 @RequiredArgsConstructor
 public class GrupoController {
 
