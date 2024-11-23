db58cbc529bf3cd82dbc5c77f63a4e44be0910de
Douglas <douglaslessat@gmail.com>
2024-11-23 16:28:06 -0300

    Docente banca

--- gtcc/src/main/java/br/gtcc/gtcc/model/mysql/DocenteBanca.java
+++ gtcc/src/main/java/br/gtcc/gtcc/model/mysql/DocenteBanca.java
@@ -1,8 +1,11 @@
 package br.gtcc.gtcc.model.mysql;
 
+import br.gtcc.gtcc.model.mysql.DocenteEnum;
 import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,7 +13,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,27 +21,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="tb_docente_banca")
+@Table(name = "tb_docente_banca")
 public class DocenteBanca {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    
+
     @NonNull
     @Column(name = "ativo")
     private Integer ativo;
-    
-    @ManyToOne(targetEntity=Banca.class, fetch=FetchType.EAGER)
-	@JoinColumn(name="id_banca")
+
+    @ManyToOne(targetEntity = Banca.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_banca")
     private Banca banca;
-    
-    @ManyToOne(targetEntity=Usuario.class, fetch=FetchType.EAGER)
-	@JoinColumn(name="id_docente")
-    private Usuario docente;
-    
-    @ManyToOne(targetEntity=TipoDocente.class, fetch=FetchType.EAGER)
-	@JoinColumn(name="id_tipo_docente")
-    private TipoDocente tipoDocente;
+
+    @ManyToOne(targetEntity = Usuario.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_docente")
+    private DocenteEnum tipoDocente;
 }
